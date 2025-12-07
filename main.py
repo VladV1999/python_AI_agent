ffe3c591c3a02a42be8a8dc413b6db294b95099a
Vlad <vlad.voskanyan@gmail.com>
2025-12-06 23:56:20 -0500

    Add basic functionality

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from python-ai-agent!")
+import os
+from google import genai
+from dotenv import load_dotenv
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if api_key == None:
+        raise RuntimeError("No api key found, or it is None")
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=prompt
+    )
+    if response.usage_metadata == None:
+        raise RuntimeError("the usage metadata is empty")
+    
+    prompt_tokens_number = response.usage_metadata.prompt_token_count
+    response_tokens_number = response.usage_metadata.candidates_token_count
+    response_text = response.text
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {prompt_tokens_number}")
+    print(f"Response tokens: {response_tokens_number}")
+    print(f"Response: \n{response_text}")
 
 if __name__ == "__main__":
     main()
