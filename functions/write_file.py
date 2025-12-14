81e26df01b4a06a82829c3e4116ecdb817a4eb12
Vlad <vlad.voskanyan@gmail.com>
2025-12-14 16:38:37 -0500

    Add basic write functionality

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,8 +1,19 @@
 import os
 def write_file(working_directory, file_path, content):
-    working_dir_abs = os.path.abspath(working_directory)
-    target_file_path = os.path.abspath(os.path.join(working_dir_abs, file_path))
-
-    if not target_file_path.startswith(working_dir_abs):
-        return f'Error: Cannote write to "{file_path}" as it is outside the permitted working directory'
-    
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_file_path = os.path.abspath(os.path.join(working_dir_abs, file_path))
+        if not target_file_path.startswith(working_dir_abs):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(target_file_path):
+            try:
+                os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
+            except Exception as e:
+                return f"Error: creating directory: {e}"
+        if os.path.exists(target_file_path) and os.path.isdir(target_file_path):
+            return f'Error: "{file_path} is a directory, not a file'
+        with open(target_file_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f'Error: {e}'
